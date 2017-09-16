0370ce2eacef070ac6cef23cd44277a56e8b6acd
Shiro Kawai <shiro@acm.org>
2017-09-15 19:04:45 -1000

    Some option parsing

--- get-gauche.sh
+++ get-gauche.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 # Ensure Gauche availability
 # https://github.com/shirok/get-gauche/README
 
 function usage() {
-    echo <<EOF
+    cat <<"EOF"
 Usage:
     get-gauche.sh [--system|--home|--current|--prefix PREFIX]
                   [--version VERSION][--check-only][--force]
@@ -35,11 +37,11 @@ Options:
 
     --version VERSION
         specify the desired version of Gauche.  VERSION can be a version
-        string (e.g. `0.9.5'), or either `latest' or `snapshot'.  The word
-        `latest' picks the latest release.  The word `snapshot' picks the
+        string (e.g. '0.9.5'), or either 'latest' or 'snapshot'.  The word
+        'latest' picks the latest release.  The word 'snapshot' picks the
         newest snapshot tarball if there's any newer than the latest
         release, or the latest release otherwise.
-        By default, `latest' is assumed.
+        By default, 'latest' is assumed.
 EOF
 }
 
@@ -53,3 +55,50 @@ function cleanup {
 }
 
 trap cleanup EXIT
+
+################################################################
+# main entry point
+#
+
+prefix=$HOME
+version=latest
+check_only=no
+force=no
+
+while test $# != 0
+do
+    case $1 in
+        --*=?*)
+            option=`expr "X$1" : 'X\([^=]*\)='`
+            optarg=`expr "X$1" : 'X[^=]*=\(.*\)'`
+            extra_shift=:
+            ;;
+        --*=)
+            option=`expr "X$1" : 'X\([^=]*\)='`
+            optarg=
+            extra_shift=:
+            ;;
+        *)
+            option=$1
+            optarg=$2
+            extra_shift=shift
+            ;;
+    esac
+    
+    case $option in
+        --system)   prefix=/usr ;;
+        --home)     prefix=$HOME ;;
+        --current)  prefix=`pwd` ;;
+        --prefix)   prefix=$optarg; $extra_shift ;;
+
+        --version)  version=$optarg; $extra_shift ;;
+        
+        --check-only) check_only=yes ;;
+        --force)      force=yes ;;
+
+        *) echo "bong"; usage; exit 1;;
+    esac
+    shift
+done
+
+
